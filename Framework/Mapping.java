24168c729a5c37e56a22413ab2ad0ed1673c849e
sarobidy13 <sarobidyfitahiana01@gmail.com>
2023-07-07 21:00:14 +0200

    sprint4

--- Framework/Mapping.java
+++ Framework/Mapping.java
@@ -1,21 +1,19 @@
 package etu1816.framework;
 
+
 public class Mapping {
-    String className;
-    String method;
+    private String className;
+    private String method;
 
     public String getClassName() {
         return className;
     }
-
     public void setClassName(String className) {
         this.className = className;
     }
-
     public String getMethod() {
         return method;
     }
-
     public void setMethod(String method) {
         this.method = method;
     }
